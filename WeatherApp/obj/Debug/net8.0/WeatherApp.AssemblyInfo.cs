1444c2f919e4bb27ab6839b34b33d18e6533adf3
Chulumanco <c.mchiza@mfundopedia.co.za>
2024-03-08 10:55:20 +0200

    add dbcontext

--- WeatherApp/obj/Debug/net8.0/WeatherApp.AssemblyInfo.cs
+++ WeatherApp/obj/Debug/net8.0/WeatherApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1edcbcde3704b631803241b91a8462e5fd9241ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4216d7c8c0abe866767e1fb489ae1d776733862e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
